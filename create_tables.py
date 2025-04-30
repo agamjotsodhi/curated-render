d1ae8e95c9a93ca0eefba1026148c2ef0a9479ef
Agamjot Sodhi <agamjotsodhi@hotmail.com>
2025-04-29 20:24:50 -0400

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -1,9 +1,7 @@
-"""   Create_tables.py
-  
-- Create PSQL Tables script
-- To run application with database:
-- Open Terminal
-- Type in:  python create_tables.py """
+# create_tables.py
+
+from dotenv import load_dotenv
+load_dotenv()  # Loads environment variables from .env
 
 from app import app
 from models import db
@@ -11,6 +9,3 @@ from models import db
 with app.app_context():
     db.create_all()
     print("Tables created successfully.")
-
-
-
